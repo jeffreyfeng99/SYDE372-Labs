f4af1327d528791bb90c286b5a5a790b96c1f651
jeffreyfeng99 <jeffreyfeng99@gmail.com>
2020-02-07 01:14:56 -0500

    optimize MED

--- Lab 1/MED_V2.m
+++ Lab 1/MED_V2.m
@@ -1,12 +1,7 @@
 function decision_grid = MED_V2(mu, X, Y)
 
-decision_grid = zeros([size(X),size(mu,3)]);
+X = repmat(X,[1,1,size(mu,3)]);
+Y = repmat(Y,[1,1,size(mu,3)]);
+decision_grid = (X-mu(1,1,:)).^2 + (Y-mu(1,2,:)).^2;
 
-for i = 1:size(X,1)
-   for j = 1:size(X,2)
-       for k = 1:size(mu,3)
-            decision_grid(i,j,k) = [X(i,j)-mu(1,1,k) Y(i,j)-mu(1,2,k)]*[X(i,j)-mu(1,1,k) Y(i,j)-mu(1,2,k)]';
-       end
-   end
-end
 end
