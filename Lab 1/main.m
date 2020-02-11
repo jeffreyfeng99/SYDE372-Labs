49f8c69304018b7b8ca791b9222d18f5af7d89fd
jjkuang <sam.joey.kuang@gmail.com>
2020-02-10 23:20:57 -0500

    remove sum metrics from confusion charts and added line to save charts as png

--- Lab 1/main.m
+++ Lab 1/main.m
@@ -52,21 +52,21 @@ case1_MAP = classify_data_V2(case1_MAP, true);
 case2_MAP = MAP_V2(c2_mu,c2_covar,[n_C n_D n_E],case2_X,case2_Y);
 case2_MAP = classify_data_V2(case2_MAP, true);
 
-% NN Decision Boundary for case 1
-case1_NN = kNN_V2({data_A,data_B},case1_X,case1_Y,1);
-case1_NN = classify_data_V2(case1_NN);
-
-% NN Decision Boundary for case 2
-case2_NN = kNN_V2({data_C,data_D,data_E},case2_X,case2_Y,1);
-case2_NN = classify_data_V2(case2_NN);
-
-% 5NN Decision Boundary for case 1
-case1_5NN = kNN_V2({data_A,data_B},case1_X,case1_Y,5);
-case1_5NN = classify_data_V2(case1_5NN);
-
-% 5NN Decision Boundary for case 2
-case2_5NN = kNN_V2({data_C,data_D,data_E},case2_X,case2_Y,5);
-case2_5NN = classify_data_V2(case2_5NN);
+% % NN Decision Boundary for case 1
+% case1_NN = kNN_V2({data_A,data_B},case1_X,case1_Y,1);
+% case1_NN = classify_data_V2(case1_NN);
+% 
+% % NN Decision Boundary for case 2
+% case2_NN = kNN_V2({data_C,data_D,data_E},case2_X,case2_Y,1);
+% case2_NN = classify_data_V2(case2_NN);
+% 
+% % 5NN Decision Boundary for case 1
+% case1_5NN = kNN_V2({data_A,data_B},case1_X,case1_Y,5);
+% case1_5NN = classify_data_V2(case1_5NN);
+% 
+% % 5NN Decision Boundary for case 2
+% case2_5NN = kNN_V2({data_C,data_D,data_E},case2_X,case2_Y,5);
+% case2_5NN = classify_data_V2(case2_5NN);
 
 %% Get confusion matrix and experimental error for each classifier
 
@@ -90,7 +90,7 @@ error_case_defn;
 % Contour colours:
 % GED: #661010
 % MED: #00ffdd
-% MAP: #ff006f (-- marker for Case 1 GED, MAP, MED)
+% MAP: #ffaa00 (-- marker for Case 1 GED, MAP, MED)
 % NN:  #495fd1
 % 5NN: #e46aeb
 % 
@@ -119,7 +119,7 @@ xlabel('x1'); ylabel('x2')
 % Plot decision boundary and equiprobability ellipse
 contour(case1_X,case1_Y,case1_GED,[0.5,0.5], 'LineColor', '#661010', 'LineWidth',1);
 contour(case1_X,case1_Y,case1_MED,[0.5,0.5], 'LineColor', '#00ffdd');
-contour(case1_X,case1_Y,case1_MAP,[0.5,0.5], '--', 'LineColor', '#ff006f');
+contour(case1_X,case1_Y,case1_MAP,[0.5,0.5], '--', 'LineColor', '#ffaa00');
 plot_ellipse(mu_A(1),mu_A(2),theta_A,sqrt(eval_A(1,1)),sqrt(eval_A(2,2)), '#D95319');
 plot_ellipse(mu_B(1),mu_B(2),theta_B,sqrt(eval_B(1,1)),sqrt(eval_B(2,2)), '#ce52ff');
 
@@ -142,25 +142,25 @@ set(gcf, 'Units', 'Normalized', 'OuterPosition', [0 0 1 1]);
 hold off
 axis equal
 
-figure
-hold on
-title('Case 1: Nearest Neighbour and 5-Nearest Neighbour decision boundaries');
-xlabel('x1'); ylabel('x2')                     
-
-% Plot decision boundary
-contour(case1_X,case1_Y,case1_NN,[0.5,0.5], 'LineColor', '#495fd1');
-contour(case1_X,case1_Y,case1_5NN,[0.5,0.5], 'LineColor', '#e46aeb');
-
-% Scatter data points
-scatter(data_A(:,1),data_A(:,2),sz,'x', 'MarkerEdgeColor', '#EDB120')
-scatter(data_B(:,1),data_B(:,2),sz,'o', 'MarkerEdgeColor', '#7E2F8E')
-
-c1_lg_2 = legend('NN boundary', '5-NN boundary', 'Data A', 'Data B', ...
-                 'Location', 'northwest');
-title(c1_lg_2, 'Legend')
-set(gcf, 'Units', 'Normalized', 'OuterPosition', [0 0 1 1]);
-hold off
-axis equal
+% figure
+% hold on
+% title('Case 1: Nearest Neighbour and 5-Nearest Neighbour decision boundaries');
+% xlabel('x1'); ylabel('x2')                     
+% 
+% % Plot decision boundary
+% contour(case1_X,case1_Y,case1_NN,[0.5,0.5], 'LineColor', '#495fd1');
+% contour(case1_X,case1_Y,case1_5NN,[0.5,0.5], 'LineColor', '#e46aeb');
+% 
+% % Scatter data points
+% scatter(data_A(:,1),data_A(:,2),sz,'x', 'MarkerEdgeColor', '#EDB120')
+% scatter(data_B(:,1),data_B(:,2),sz,'o', 'MarkerEdgeColor', '#7E2F8E')
+% 
+% c1_lg_2 = legend('NN boundary', '5-NN boundary', 'Data A', 'Data B', ...
+%                  'Location', 'northwest');
+% title(c1_lg_2, 'Legend')
+% set(gcf, 'Units', 'Normalized', 'OuterPosition', [0 0 1 1]);
+% hold off
+% axis equal
 
 % Plotting Case 2
 figure
@@ -171,7 +171,7 @@ xlabel('x1'); ylabel('x2')
 % Plot decision boundary and equiprobability ellipse
 contour(case2_X,case2_Y,case2_GED, [0.5,1.5], 'LineColor', '#661010');
 contour(case2_X,case2_Y,case2_MED, [0.5,1.5], 'LineColor', '#00ffdd');
-contour(case2_X,case2_Y,case2_MAP, [0.5,1.5], 'LineColor', '#ff006f');
+contour(case2_X,case2_Y,case2_MAP, [0.5,1.5], 'LineColor', '#ffaa00');
 plot_ellipse(mu_C(1),mu_C(2),theta_C,sqrt(eval_C(1,1)),sqrt(eval_C(2,2)),'g');
 plot_ellipse(mu_D(1),mu_D(2),theta_D,sqrt(eval_D(1,1)),sqrt(eval_D(2,2)),'b');
 plot_ellipse(mu_E(1),mu_E(2),theta_E,sqrt(eval_E(1,1)),sqrt(eval_E(2,2)),'r');
@@ -198,25 +198,25 @@ title(c2_lg_1, 'Legend')
 set(gcf, 'Units', 'Normalized', 'OuterPosition', [0 0 1 1]);
 hold off
 
-figure
-hold on
-title('Case 2: Nearest Neighbour and 5-Nearest Neighbour decision boundaries');
-xlabel('x1'); ylabel('x2')  
-
-% Plot decision boundary
-contour(case2_X,case2_Y,case2_NN,[0.5,0.5], 'LineColor', '#495fd1');
-contour(case2_X,case2_Y,case2_5NN,[0.5,0.5], 'LineColor', '#e46aeb');
-
-% Scatter data points
-scatter(data_C(:,1),data_C(:,2),sz,'+', 'MarkerEdgeColor', '#77AC30')
-scatter(data_D(:,1),data_D(:,2),sz,'o', 'MarkerEdgeColor', '#4DBEEE')
-scatter(data_E(:,1),data_E(:,2),sz,'x', 'MarkerEdgeColor', '#A2142F')
-
-c2_lg_2 = legend('NN boundary', '5-NN boundary', 'Data C', 'Data D', 'Data E', ...
-                 'Location', 'northwest');
-title(c2_lg_2, 'Legend')
-set(gcf, 'Units', 'Normalized', 'OuterPosition', [0 0 1 1]);
-hold off
-axis equal
+% figure
+% hold on
+% title('Case 2: Nearest Neighbour and 5-Nearest Neighbour decision boundaries');
+% xlabel('x1'); ylabel('x2')  
+% 
+% % Plot decision boundary
+% contour(case2_X,case2_Y,case2_NN,[0.5,0.5], 'LineColor', '#495fd1');
+% contour(case2_X,case2_Y,case2_5NN,[0.5,0.5], 'LineColor', '#e46aeb');
+% 
+% % Scatter data points
+% scatter(data_C(:,1),data_C(:,2),sz,'+', 'MarkerEdgeColor', '#77AC30')
+% scatter(data_D(:,1),data_D(:,2),sz,'o', 'MarkerEdgeColor', '#4DBEEE')
+% scatter(data_E(:,1),data_E(:,2),sz,'x', 'MarkerEdgeColor', '#A2142F')
+% 
+% c2_lg_2 = legend('NN boundary', '5-NN boundary', 'Data C', 'Data D', 'Data E', ...
+%                  'Location', 'northwest');
+% title(c2_lg_2, 'Legend')
+% set(gcf, 'Units', 'Normalized', 'OuterPosition', [0 0 1 1]);
+% hold off
+% axis equal
 %%
 plot_confusion_charts;
