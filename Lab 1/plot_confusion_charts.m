0b28e5c1379c7077946b23ebb4cbfe5e805d2172
jjkuang <sam.joey.kuang@gmail.com>
2020-02-10 23:46:22 -0500

    line for saving confusion charts

--- Lab 1/plot_confusion_charts.m
+++ Lab 1/plot_confusion_charts.m
@@ -1,42 +1,44 @@
 case1_labels = ['A';'B'];
 case2_labels = ['C';'D';'E'];
 
+filepath = fullfile(strcat(pwd,'/confusion_charts/'));
+
 figure
 confusionchart(C1_MED, case1_labels, 'Title', 'Case 1 MED');
-saveas(gcf, 'confusion_charts/C1_MED.png');
+saveas(gcf, strcat(filepath,'C1_MED'),'png');
 
 figure
 confusionchart(C1_GED, case1_labels, 'Title', 'Case 1 GED');
-saveas(gcf, 'confusion_charts/C1_GED.png');
+saveas(gcf, strcat(filepath,'C1_GED'),'png');
 
 figure
 confusionchart(C1_MAP, case1_labels, 'Title', 'Case 1 MAP');
-saveas(gcf, 'confusion_charts/C1_MAP.png');
+saveas(gcf, strcat(filepath,'C1_MAP'),'png');
 
 figure
 confusionchart(C1_NN, case1_labels, 'Title', 'Case 1 NN');
-saveas(gcf, 'confusion_charts/C1_NN.png');
+saveas(gcf, strcat(filepath,'C1_NN'),'png');
 
 figure
 confusionchart(C1_5NN, case1_labels, 'Title', 'Case 1 5NN');
-saveas(gcf, 'confusion_charts/C1_5NN.png');
+saveas(gcf, strcat(filepath,'C1_5NN'),'png');
 
 figure
 confusionchart(C2_MED, case2_labels, 'Title', 'Case 2 MED');
-saveas(gcf, 'confusion_charts/C2_MED.png');
+saveas(gcf, strcat(filepath,'C2_MED'),'png');
 
 figure
 confusionchart(C2_GED, case2_labels, 'Title', 'Case 2 GED');
-saveas(gcf, 'confusion_charts/C2_GED.png');
+saveas(gcf, strcat(filepath,'C2_GED'),'png');
 
 figure
 confusionchart(C2_MAP, case2_labels, 'Title', 'Case 2 MAP');
-saveas(gcf, 'confusion_charts/C2_MAP.png');
+saveas(gcf, strcat(filepath,'C2_MAP'),'png');
 
 figure
 confusionchart(C2_NN, case2_labels, 'Title', 'Case 2 NN');
-saveas(gcf, 'confusion_charts/C2_NN.png');
+saveas(gcf, strcat(filepath,'C2_NN'),'png');
 
 figure
 confusionchart(C2_5NN, case2_labels, 'Title', 'Case 2 5NN');
-saveas(gcf, 'confusion_charts/C2_5NN.png');
+saveas(gcf, strcat(filepath,'C2_5NN'),'png');
