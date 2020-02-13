39e41720ac140cffae5a7a6739ef6304c6013764
jeffreyfeng99 <jeffreyfeng99@gmail.com>
2020-02-12 23:14:19 -0500

    optimization

--- Lab 1/kNN.m
+++ Lab 1/kNN.m
@@ -17,12 +17,8 @@ for i = 1:size(X,1)
    for j = 1:size(X,2)
        for m = 1:length(data)
            % get Class Prototype
-           distances = zeros(size(data{m},1),1);
            % get distance between (X,Y) point and all Class sample points
-           for a = 1:size(data{m},1)
-               dist = norm([X(i,j) Y(i,j)] - data{m}(a,:));
-               distances(a) = dist;         
-           end
+           distances = sqrt((X(i,j)- data{m}(:,1)).^2 + (Y(i,j)-data{m}(:,2)).^2);
 
            % sort distance array from shortest to longest 
            sorted_distances = sort(distances);
