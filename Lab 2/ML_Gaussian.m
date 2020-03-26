57f843a5d986a91c9e6cdd6607b95d7a467af8e8
jeffreyfeng99 <jeffreyfeng99@gmail.com>
2020-03-25 21:15:54 -0400

    optimize

--- Lab 2/ML_Gaussian.m
+++ Lab 2/ML_Gaussian.m
@@ -14,17 +14,26 @@ function decision_grid = ML_Gaussian(mu,covar, X, Y)
 
 decision_grid = zeros([size(X),size(mu,3)]);
 
-for i = 1:size(X,1)
-   for j = 1:size(X,2)
-       for k = 1:size(mu,3)
-           XY_mu_ = [X(i,j)-mu(1,1,k) Y(i,j)-mu(2,1,k)];
-           exp_term = exp(-1/2*(XY_mu_*(covar(:,:,k)^(-1))*XY_mu_'));
-           
-           decision_grid(i,j,k) = (exp_term)...
-                                /((2*pi)*sqrt(det(covar(:,:,k))));
-       end
-   end
+% Efficienct (use mvnpdf)
+XY_mu_ = [X(:) Y(:)];
+for k = 1:size(mu,3)
+    distribution = mvnpdf(XY_mu_, mu(:,1,k)', covar(:,:,k));
+    distribution = reshape(distribution, size(X,1),size(X,2));
+    decision_grid(:,:,k) = distribution;
 end
+
+% Inefficient (iterate)
+% for i = 1:size(X,1)
+%    for j = 1:size(X,2)
+%        for k = 1:size(mu,3)
+%            XY_mu_ = [X(i,j)-mu(1,1,k) Y(i,j)-mu(2,1,k)];
+%            exp_term = exp(-1/2*(XY_mu_*(covar(:,:,k)^(-1))*XY_mu_'));
+%            
+%            decision_grid(i,j,k) = (exp_term)...
+%                                 /((2*pi)*sqrt(det(covar(:,:,k))));
+%        end
+%    end
+% end
 
 decision_grid = classify_data(decision_grid, true);
 
